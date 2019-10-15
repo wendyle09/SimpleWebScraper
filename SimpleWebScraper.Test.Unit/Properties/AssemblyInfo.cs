1aee7aacce2b61d39c1c43a51414f9e619cfe162
Wendy Le <wendyle09@gmail.com>
2019-10-14 19:33:00 -0700

    Added test units

---
+++ SimpleWebScraper.Test.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleWebScraper.Test.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleWebScraper.Test.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3f453ec-f4bc-4a5a-9ae6-2a6306de536c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SimpleWebScraper.Test.Unit/Workers/ScraperTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleWebScraper.Builders;
+using SimpleWebScraper.Data;
+using SimpleWebScraper.Workers;
+
+namespace SimpleWebScraper.Test.Unit
+{
+    [TestClass]
+    public class ScraperTest
+    {
+        private readonly Scraper _scraper = new Scraper();
+
+        [TestMethod]
+        public void FindCollectionWithNoParts()
+        {
+            string content = "Lorem ipsum <a href=\"http://domain.com\" data-id=\"dataID\" class=\"result-title hdrlnk\">Item Description</a> dolor sit amet";
+
+            ScrapeCriteria scrapeCriteria = new ScrapeCriteriaBuilder()
+                .WithData(content)
+                .WithRegex(@"<a href=\""(.*?)\"" data-id=\""(.*?)\"" class=\""result-title hdrlnk\"">(.*?)</a>")
+                .WithRegexOption(RegexOptions.ExplicitCapture)
+                .Build();
+
+            var foundElements = _scraper.Scrape(scrapeCriteria);
+
+            Assert.IsTrue(foundElements.Count == 1);
+        }
+
+        public void FindCollectionWithTwoParts()
+        {
+            string content = "Lorem ipsum <a href=\"http://domain.com\" data-id=\"dataID\" class=\"result-title hdrlnk\">Item Description</a> dolor sit amet ";
+
+            ScrapeCriteria scrapeCriteria = new ScrapeCriteriaBuilder()
+                        .WithData(content)
+                        // RegEx for entire listing element
+                        .WithRegex(@"<a href=\""(.*?)\"" data-id=\""(.*?)\"" class=\""result-title hdrlnk\"">(.*?)</a>")
+                        .WithRegexOption(RegexOptions.ExplicitCapture)
+                        // Build scraper for listing description part
+                        .WithPart(new ScrapeCriteriaPartBuilder()
+                            .WithRegex(@">(.*?)</a>")
+                            .WithRegexOption(RegexOptions.Singleline)
+                            .Build())
+                        // Build scraper for listing URL part
+                        .WithPart(new ScrapeCriteriaPartBuilder()
+                            .WithRegex(@"href=\""(.*?)\""")
+                            .WithRegexOption(RegexOptions.Singleline)
+                            .Build())
+                        .Build();
+
+            var foundElements = _scraper.Scrape(scrapeCriteria);
+
+            Assert.IsTrue(foundElements.Count == 2);
+            Assert.IsTrue(foundElements[0] == "Item Description");
+            Assert.IsTrue(foundElements[1] == "http://domain.com");
+        }
+    }
+}
